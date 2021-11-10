0c7591f32fd557ff0efc6b0eff509520155ec294
Artur <artur133715@gmail.com>
2021-11-11 00:16:19 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import './components/Store/css/index.css';
+import PropTypes from 'prop-types';
+import StoreModel from "./models/StoreModel";
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const item = {
+        brand: 'Tiger of Sweden',
+        title: 'Leonard coat',
+        description: 'Minimalistic coat in cotton-blend',
+        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+        price: 399,
+        currency: '£'
+    }
+
+    class ShopItemClass extends React.Component {
+        constructor(props) {
+            super(props);
+        }
+
+        render() {
+            const { item } = this.props;
+            return (
+                <div className="main-content">
+                    <h2>{item.brand}</h2>
+                    <h1>{item.title}</h1>
+                    <h3>{item.description}</h3>
+                    <div className="description">
+                        {item.descriptionFull}
+                    </div>
+                    <div className="highlight-window mobile">
+                        <div className="highlight-overlay"></div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="purchase-info">
+                        <div className="price">{item.currency}{item.price}</div>
+                        <button>Добавить в корзину</button>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    function ShopItemFunc({item}) {
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="background-element">
+
+                </div>
+                <div className="highlight-window">
+                    <div className='highlight-overlay'>
+
+                    </div>
+                </div>
+                <div className="window">
+                  <ShopItemFunc item={item} />
+                </div>
+            </div>
+            <div className="container">
+                <div className="background-element">
+                </div>
+                <div className="highlight-window">
+                    <div className='highlight-overlay'>
+
+                    </div>
+                </div>
+                <div className="window">
+                    <ShopItemClass item={item} />
+                </div>
+            </div>
+        </>
+    );
+}
+
+StoreModel.propTypes = {
+    item: PropTypes.instanceOf(StoreModel).isRequired
 }
 
 export default App;
